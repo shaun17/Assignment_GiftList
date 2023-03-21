4c49e2b96137eb4f3e27d1a29955e810571058b6
shaun <shaun@shaun-MacBook-Pro.local>
2023-03-21 08:44:06 +0800

    do assignment

--- client/index.js
+++ client/index.js
@@ -6,9 +6,15 @@ const serverUrl = 'http://localhost:1225';
 
 async function main() {
   // TODO: how do we prove to the server we're on the nice list? 
+  const merkletree = new MerkleTree(niceList);
+  const root = merkletree.getRoot();
+  let index = 10;
+  const proof = merkletree.getProof(index);
 
   const { data: gift } = await axios.post(`${serverUrl}/gift`, {
     // TODO: add request body parameters here!
+    proof: proof,
+    leaf : niceList[10]
   });
 
   console.log({ gift });
